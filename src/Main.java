8cb0ccab305b338d69f579e4f72730a67b38c6ad
Nahid_Hasan <nahid8072@gmail.com>
2024-09-08 20:10:32 +0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner choiceInput = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("===WELCOME===");
+        System.out.println("1. Show Cars");
+        System.out.println("2. Rent a Car");
+        System.out.println("3. Exit");
+
+        int choice = choiceInput.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Choice 1 has been selected");
+                break;
+            case 2:
+                System.out.println("Choice 2 has been selected");
+                break;
+            case 3:
+                System.out.println("Choice 3 has been selected");
+                break;
+            default:
+                System.out.println("No option selected");
         }
+
     }
 }
